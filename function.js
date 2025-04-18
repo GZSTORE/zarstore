8322e2176ef1810e76ff1c74847e2b08dd877a37
Fahri-Hosting <147958381+GZSTORE@users.noreply.github.com>
2025-04-18 21:28:11 +0700

    function.js

---
+++ function.js
@@ -0,0 +1,10 @@
+// Numbers
+
+
+// 1. Receives 2 numbers as parameters and returns their sum.  
+function sum(nr1, nr2) {
+    return nr1 + nr2;
+};
+
+// 2. Receives 2 numbers as parameters: the gross salary of an employee for each month, 
+// and the tax percentage he has to pay from that. 
